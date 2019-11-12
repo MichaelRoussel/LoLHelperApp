55a415afa92892bcc66c12797affa7cf9695f76f
Michael Roussel <pokemon_pidgeot@msn.com>
2019-11-12 07:31:54 -0500

    Finished all layouts, added navigation between layouts

--- app/src/main/java/com/example/lolhelperapp/LandingActivity.kt
+++ app/src/main/java/com/example/lolhelperapp/LandingActivity.kt
@@ -1,15 +1,49 @@
 package com.example.lolhelperapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_landing.*
 
 class LandingActivity : AppCompatActivity() {
+
+    //create db instance to grab users and other info
+    val db = FirebaseFirestore.getInstance()
+
+    override fun onStart() {
+        super.onStart()
+
+        //make sure there is an authenticated user on start, if not redirect to signin
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user == null) {
+            val intent = Intent(applicationContext, SignInActivity::class.java)
+            startActivity(intent)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing)
+
+        //sign out button
+        fabExitLanding.setOnClickListener{
+            AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener {
+                    //redirect to sign in after logging out
+                    val intent = Intent(applicationContext, SignInActivity::class.java)
+                    startActivity(intent)
+                }
+        }
+
+        //profile button
+        fabProfile.setOnClickListener{
+            val intent = Intent(applicationContext, ProfileActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
--- app/src/main/java/com/example/lolhelperapp/ProfileActivity.kt
+++ app/src/main/java/com/example/lolhelperapp/ProfileActivity.kt
@@ -1,15 +1,59 @@
 package com.example.lolhelperapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_champion.*
+import kotlinx.android.synthetic.main.profile_activity.*
 
 class ProfileActivity : AppCompatActivity() {
+
+    //create db instance to grab users and other info
+    val db = FirebaseFirestore.getInstance()
+
+//    override fun onStart() {
+//        super.onStart()
+//
+//        //make sure there is an authenticated user on start, if not redirect to signin
+//        val user = FirebaseAuth.getInstance().currentUser
+//        if (user == null) {
+//            val intent = Intent(applicationContext, SignInActivity::class.java)
+//            startActivity(intent)
+//        }
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.profile_activity)
+
+        val user = FirebaseAuth.getInstance().currentUser
+
+        user?.let {
+            val email = user.email
+            textViewEmail.text = email
+        }
+
+
+        //sign out button
+        fabExitProfile.setOnClickListener{
+            AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener {
+                    //redirect to sign in after logging out
+                    val intent = Intent(applicationContext, SignInActivity::class.java)
+                    startActivity(intent)
+                }
+        }
+
+        //back button
+        fabHome.setOnClickListener{
+            val intent = Intent(applicationContext, LandingActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
