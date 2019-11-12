e254eddc7e380c267f9d8bff762d34ed0bbc8e8e
Drake <drake,esdon@hotmail.com>
2019-11-12 00:03:30 -0500

    Added landing  Layout
    layout for landing page

---
+++ app/src/main/java/com/example/lolhelperapp/LandingActivity.kt
@@ -0,0 +1,15 @@
+package com.example.lolhelperapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class LandingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing)
+
+    }
+}
