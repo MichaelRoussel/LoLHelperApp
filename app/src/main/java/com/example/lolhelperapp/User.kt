bbf75c907a6512384c11a0f605cb36c3db30cf7b
Drake <drake,esdon@hotmail.com>
2019-11-11 23:16:44 -0500

    Added Profile Layout
    finished the gui for my profile

--- app/src/main/java/com/example/lolhelperapp/User.kt
+++ app/src/main/java/com/example/lolhelperapp/User.kt
@@ -1,2 +1,26 @@
 package com.example.lolhelperapp
 
+class User {
+    var id: String? = null
+    var userName: String? = null
+    var perfeeredChampOne: String? = null
+    var perfeeredChampTwo: String? = null
+    var perfeeredChampThree: String? = null
+    var perfeeredChampFour: String? = null
+
+    constructor() {
+
+    }
+
+
+    constructor(id: String, userName: String, perfeeredChampOne: String, perfeeredChampTwo: String, perfeeredChampThree: String, perfeeredChampFour: String) {
+        this.userName = userName
+        this.id = id
+        this.perfeeredChampOne = perfeeredChampOne
+        this.perfeeredChampTwo = perfeeredChampTwo
+        this.perfeeredChampThree = perfeeredChampThree
+        this.perfeeredChampFour = perfeeredChampFour
+
+    }
+
+}
